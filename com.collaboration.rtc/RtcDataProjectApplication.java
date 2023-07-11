03974a88f4e5de1620774019800957aaf7b20852
Razaq <captain.alabi@gmail.com>
2023-07-11 12:30:39 +0100

    Create RtcDataProjectApplication.java

---
+++ com.collaboration.rtc/RtcDataProjectApplication.java
@@ -0,0 +1,13 @@
+package com.collaboration.rtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtcDataProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtcDataProjectApplication.class, args);
+	}
+
+}
